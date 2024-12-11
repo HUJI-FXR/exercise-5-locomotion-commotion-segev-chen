d900358afe5c1612a8d965704be77db9c6191ba1
Segev Chen <121970894+segev-chen@users.noreply.github.com>
2024-12-11 17:06:05 +0200

    commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 5f;
+    void Update()
+    {
+        transform.Translate(transform.forward * Time.deltaTime * speed,
+            Space.World);
+        if (transform.position.z <= Camera.main.transform.position.z)
+        {
+            FindObjectOfType<GameManagerScript>().EndLife();
+        }
+    }
+
+    public void DestroyBallTemp()
+    {
+        Destroy(gameObject);
+    }
+
+    //public void HitBall()
+    //{
+    //    GameManagerScript.score++;
+    //    Debug.Log(GameManagerScript.score);
+    //    Destroy(gameObject);
+    //}
+
+
+
+
+}
